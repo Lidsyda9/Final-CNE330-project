aa28b6f2ec366fb06aa4ed35fc28b7c94bef1683
Lidsyda <lidsyda.npc@yahoo.com>
2024-11-28 22:51:13 -0800

    initial commit

---
+++ Basic Calculator.py
@@ -0,0 +1,44 @@
+#Basic Calculator: Build a calculator that supports operations like addition, subtraction, multiplication, and division.
+# 1. Add
+# 2. Subtract
+# 3. Multiply
+# 4. Divide
+
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error! Division by zero."
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    while True:
+        choice = input("Enter choice (1/2/3/4): ")
+        if choice in ['1', '2', '3', '4']:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            if choice == '1':
+                print(f"{num1} + {num2} = {add(num1, num2)}")
+            elif choice == '2':
+                print(f"{num1} • {num2} = {subtract(num1, num2)}")
+            elif choice == '3':
+                print(f"{num1} * {num2} = {multiply(num1, num2)}")
+            elif choice == '4':
+                print(f"{num1} / {num2} = {divide(num1, num2)}")
+            # Ask if the user wants to perform another calculation
+            next_calculation = input("Do you want to perform another calculation? (yes/no): ")
+            if next_calculation.lower() != 'yes':
+                break
+        else:
+            print("Invalid input")
+if __name__ == "__main__":
+    calculator()
